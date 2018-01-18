211da6f828e146098dce72c511f1d445837e7f3b
Pablo <pabete.l@gmail.com>
2018-01-18 10:33:37 +0100

    Subo el menu

---
+++ js/menu.js
@@ -0,0 +1,41 @@
+var app = new PIXI.Application(800, 600);
+document.body.appendChild(app.view);
+
+var bg = PIXI.Sprite.fromImage('img/FondoMenu.jpg');
+bg.width = app.screen.width;
+bg.height = app.screen.height;
+app.stage.addChild(bg);
+
+var megaManTitle = PIXI.Sprite.fromImage('img/Titulo.png');
+megaManTitle.x = (app.screen.width / 2);
+megaManTitle.y = 0;
+megaManTitle.anchor.set(0.5, 0,5);
+app.stage.addChild(megaManTitle);
+
+var ready = PIXI.Sprite.fromImage('img/Iniciar.png');
+ready.x = (app.screen.width / 2);
+ready.y = 300;
+ready.anchor.set(0.5, 0,5);
+app.stage.addChild(ready);
+
+var record = PIXI.Sprite.fromImage('img/Record.png');
+record.x = (app.screen.width / 2);
+record.y = 400;
+record.anchor.set(0.5, 0,5);
+app.stage.addChild(record);
+
+// Ready onclick
+ready.interactive = true;
+ready.buttonMode = true;
+ready.on('pointerdown', onClickReady);
+function onClickReady () {
+    window.location="game.html";
+}
+//Record onclick
+record.interactive = true;
+record.buttonMode = true;
+record.on('pointerdown', onClickRecord);
+
+function onClickRecord () {
+    console.log("Record");
+}
